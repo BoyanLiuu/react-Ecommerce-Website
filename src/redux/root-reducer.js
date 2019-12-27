280928d980d62dc2ec040e0230226efd02fa8aaa
Boyan Liu <heyiamboyan@gmail.com>
2019-12-26 20:12:45 -0500

    implementing redux persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 // it combine all the other state 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
-import cartReducer from './cart/cart.reducer'
-export default combineReducers({
+import cartReducer from './cart/cart.reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  //the only thing we want to persist, we add to white list.
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+//a modified root reducer with persistance on top of it
+export default persistReducer(persistConfig, rootReducer);
